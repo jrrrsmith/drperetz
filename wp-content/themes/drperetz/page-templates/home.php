26bcc090655f4a2678c3b388a16127a15799eb9f
jrrrsmith <jeremy.scott.smith@me.com>
2016-04-11 00:14:32 -0400

    initial project files

---
+++ wp-content/themes/drperetz/page-templates/home.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Template Name: Home Page
+ */
+
+get_header(); 
+
+?>
+<section class="home-body">
+  <?php include 'page-templates/partials/hero.php'  ?>
+</section>
+<?php get_footer(); ?>
