4b28b9a14ec977299dda33ca73e7e59fe1b9564e
jrrrsmith <jeremy.scott.smith@me.com>
2016-04-11 01:24:28 -0400

    Added structure to Elements of Care section on Home Page

--- wp-content/themes/drperetz/page-templates/home.php
+++ wp-content/themes/drperetz/page-templates/home.php
@@ -10,5 +10,6 @@ get_header();
 <section class="home-body wrap">
   <?php include 'partials/hero.php'  ?>
   <?php include 'partials/home/intro.php'  ?>
+  <?php include 'partials/home/elements.php'  ?>
 </section>
 <?php get_footer(); ?>
---
+++ wp-content/themes/drperetz/page-templates/partials/home/elements.php
@@ -0,0 +1,25 @@
+<div class="elements">
+  <div class="elements__tabs">
+    <div class="element-item tab tab--active">
+      <h3 class="element-item__title">nourish - cleanse - heal</h3>
+      <img src="<?php bloginfo('template_url');?>/assets/images/care-1.png" class="element-item-img" />
+      <p class="element-item-info">Psoriasis, as with many other autoimmune diseases, is greatly affected by your diet. Being a disease of inflammation, psoriasis responds well to a diet geared toward decreasing your total body inflammation. <a href="#">Learn more...</a></p>
+      <a class="button" href="#">Schedule a Consultation</a>
+      <a class="button" href="#">Sign Up for Newsletter</a>
+    </div>
+    <div class="element-item tab">
+      <h3 class="element-item__title">nourish - cleanse - heal</h3>
+      <img src="<?php bloginfo('template_url');?>/assets/images/care-2.png" class="element-item-img" />
+      <a class="button" href="#">Schedule a Consultation</a>
+      <p class="element-item-info">Psoriasis, as with many other autoimmune diseases, is greatly affected by your diet. Being a disease of inflammation, psoriasis responds well to a diet geared toward decreasing your total body inflammation. <a href="#">Learn more...</a></p>
+      <a class="button" href="#">Sign Up for Newsletter</a>
+    </div>
+    <div class="element-item tab">
+      <h3 class="element-item__title">nourish - cleanse - heal</h3>
+      <img src="<?php bloginfo('template_url');?>/assets/images/care-3.png" class="element-item-img" />
+      <a class="button" href="#">Schedule a Consultation</a>
+      <a class="button" href="#">Sign Up for Newsletter</a>
+      <p class="element-item-info">Psoriasis, as with many other autoimmune diseases, is greatly affected by your diet. Being a disease of inflammation, psoriasis responds well to a diet geared toward decreasing your total body inflammation. <a href="#">Learn more...</a></p>
+    </div>
+  </div>
+</div>
