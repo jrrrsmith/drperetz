d242a6d53fb04cff66aba2e2acb2449e77546455
jrrrsmith <jeremy.scott.smith@me.com>
2016-07-14 23:23:20 -0400

    added header sections, fixed positioning of hero

--- wp-content/themes/drperetz/header.php
+++ wp-content/themes/drperetz/header.php
@@ -1,4 +1,14 @@
 <?php include 'page-templates/partials/head.php'  ?>
-<header class="header wrap">
+<header class="header">
+  <div class="header__top">
+    <div class="header__inner wrap"></div>
+  </div>
+  <div class="header__bottom">
+    <div class="header__inner wrap">
+      <a href="<?php home_url(); ?>">
+        <img src="<?php bloginfo('template_url');?>/assets/images/logo.png" class="logo" />
+      </a>
+    </div>
+  </div>
 </header>
 <div id="page">
