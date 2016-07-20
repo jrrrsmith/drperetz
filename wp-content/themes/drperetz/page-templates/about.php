a05744484d80990513cdf922648a1d1dd08a74a0
jrrrsmith <jeremy.scott.smith@me.com>
2016-07-20 00:09:59 -0400

    added style for about page

--- wp-content/themes/drperetz/page-templates/about.php
+++ wp-content/themes/drperetz/page-templates/about.php
@@ -7,7 +7,7 @@
 get_header(); 
 
 ?>
-<section class="home-body">
+<section class="about-body">
   <div class="wrap">
     <?php include 'partials/about/intro.php'  ?>
     <?php include 'partials/about/details.php'  ?>
--- wp-content/themes/drperetz/page-templates/partials/about/details.php
+++ wp-content/themes/drperetz/page-templates/partials/about/details.php
@@ -8,10 +8,15 @@
         <?php if( have_rows('education') ): ?>
         <?php while( have_rows('education') ): the_row(); ?>
         <li class="education-item">
-          <?php the_sub_field('degree'); ?>: <?php the_sub_field('school'); ?></a>
+          <span class="italic"><?php the_sub_field('degree'); ?></span>: <?php the_sub_field('school'); ?></a>
         </li>
         <?php endwhile; ?>
         <?php endif; ?>
       </ul>
+      <div class="about-details__buttons">
+        <a href="#" class="button button--green--solid">Schedule a Consultation</a>
+        <a href="#" class="button button--green--solid">Sign Up for Newsletter</a>
+        <a href="#" class="button button--green--solid">Sign Up to Watch Videos</a>
+      </div>
   </div>
 </div>
