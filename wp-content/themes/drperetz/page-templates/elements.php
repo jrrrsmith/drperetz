b66828fe336eeea9d3b696eaaf5e3d94e6b6f909
jrrrsmith <jeremy.scott.smith@me.com>
2016-07-22 01:17:14 -0400

    sliding panels on elements of care page

--- wp-content/themes/drperetz/page-templates/elements.php
+++ wp-content/themes/drperetz/page-templates/elements.php
@@ -10,7 +10,7 @@ get_header();
 <section class="elemets-body">
   <div class="wrap">
     <?php include 'partials/elements/intro.php'  ?>
-    <?php include 'partials/elements/details.php'  ?>
   </div>
+  <?php include 'partials/elements/details.php'  ?>
 </section>
 <?php get_footer(); ?>
