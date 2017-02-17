2be26e920c0d6a9efa0a31f7b76d0024ab784340
jrrrsmith <jeremy.scott.smith@me.com>
2017-02-17 16:48:20 -0500

    new site started

--- wp-content/themes/drperetz/page-templates/partials/head.php
+++ wp-content/themes/drperetz/page-templates/partials/head.php
@@ -4,10 +4,10 @@
   <?php wp_head(); ?>
   <meta charset="UTF-8">
   <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico?v=2" />
-  <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1" >
+  <meta name="viewport" content="width=device-width, initial-scale=1" >
   <title><?php bloginfo('title'); ?></title>
-  <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300italic,700,700italic,600,600italic' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="<?php bloginfo('template_url');?>/assets/css/style.css?<?php echo time(); ?>">
+  <link href="https://fonts.googleapis.com/css?family=Alegreya:400,400i,700,700i,900,900i" rel="stylesheet">
+  <link rel="stylesheet" href="<?php bloginfo('template_url');?>/assets/css/style.min.css?<?php echo time(); ?>">
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
 <body <?php body_class(); ?>>
